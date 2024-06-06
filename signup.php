463e4892b6b76c868bb12d87398bdd65a7e4b7e6
Nur Afifah Binti Nor'Azmi <ifahfifah66@gmail.com>
2024-06-06 23:04:24 +0800

    first commit

---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./signup.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="form">
+        <div class="title>
+    <p>Sign Up form</p>
+</div>
+    <form action="" method="post">
+        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="LastName" placeholder="Last Name">
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="password" placeholder="Password">
+        <input type="text" name="confirmPassword" placeholder="Confirm Password">
+        <input type="submit" value="Sign Up" name="signUP_button">
+    </form>
+</div>
+</body>
+</html>`
